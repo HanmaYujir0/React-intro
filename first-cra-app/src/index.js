c0a1ef928bf7f48b7406994f6be84ff9d8613f6b
Magomed-Emin <sadikov.magomed@bk.ru>
2022-06-13 11:22:23 +0300

    завершил таску

---
+++ first-cra-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1> Первый релиз</h1>
+    <ul>
+      <li>Изменить первый параметр метода render</li>
+      <li>Вывести свою верстку</li>
+    </ul>
+  </div>
+);
